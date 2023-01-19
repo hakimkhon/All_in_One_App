f9016f85566f1fbbc051ea8b80330b784ade3eb9
Hakimkhon <hakimkhon94@gmail.com>
2023-01-19 17:38:12 +0500

    all in one app

---
+++ app/src/main/java/uz/hakimkhon/allinoneapp/MainActivity.java
@@ -0,0 +1,26 @@
+package uz.hakimkhon.allinoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
